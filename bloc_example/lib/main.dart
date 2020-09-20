78fa4990985e09c60a976d2e62b40bd89fa3fa8b
DanielBarragan96 <ie702554@iteso.mx>
2020-09-20 11:01:47 -0500

    ejemplo funcional usando bloc para la interfaz inicial

---
+++ bloc_example/lib/main.dart
@@ -0,0 +1,183 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:bloc/bloc.dart';
+
+class SimpleBlocObserver extends BlocObserver {
+  @override
+  void onEvent(Bloc bloc, Object event) {
+    print('bloc: ${bloc.runtimeType}, event: $event');
+    super.onEvent(bloc, event);
+  }
+
+  @override
+  void onChange(Cubit cubit, Change change) {
+    print('cubit: ${cubit.runtimeType}, change: $change');
+    super.onChange(cubit, change);
+  }
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    print('bloc: ${bloc.runtimeType}, transition: $transition');
+    super.onTransition(bloc, transition);
+  }
+
+  @override
+  void onError(Cubit cubit, Object error, StackTrace stackTrace) {
+    print('cubit: ${cubit.runtimeType}, error: $error');
+    super.onError(cubit, error, stackTrace);
+  }
+}
+
+void main() {
+  Bloc.observer = SimpleBlocObserver();
+
+  cubitMain();
+  blocMain();
+  runApp(MyApp());
+}
+
+void cubitMain() {
+  print('----------CUBIT----------');
+
+  /// Create a `CounterCubit` instance.
+  final cubit = CounterCubit();
+
+  /// Access the state of the `cubit` via `state`.
+  print(cubit.state); // 0
+
+  /// Interact with the `cubit` to trigger `state` changes.
+  cubit.increment();
+
+  /// Access the new `state`.
+  print(cubit.state); // 1
+
+  /// Close the `cubit` when it is no longer needed.
+  cubit.close();
+}
+
+void blocMain() async {
+  print('----------BLOC----------');
+
+  /// Create a `CounterBloc` instance.
+  final bloc = CounterBloc();
+
+  /// Access the state of the `bloc` via `state`.
+  print(bloc.state);
+
+  /// Interact with the `bloc` to trigger `state` changes.
+  bloc.add(CounterEvent.increment);
+
+  /// Wait for next iteration of the event-loop
+  /// to ensure event has been processed.
+  await Future<void>.delayed(Duration.zero);
+
+  /// Access the new `state`.
+  print(bloc.state);
+
+  /// Close the `bloc` when it is no longer needed.
+  await bloc.close();
+}
+
+/// A `CounterCubit` which manages an `int` as its state.
+class CounterCubit extends Cubit<int> {
+  /// The initial state of the `CounterCubit` is 0.
+  CounterCubit() : super(0);
+
+  /// When increment is called, the current state
+  /// of the cubit is accessed via `state` and
+  /// a new `state` is emitted via `emit`.
+  void increment() => emit(state + 1);
+}
+
+/// The events which `CounterBloc` will react to.
+enum CounterEvent { increment }
+
+/// A `CounterBloc` which handles converting `CounterEvent`s into `int`s.
+class CounterBloc extends Bloc<CounterEvent, int> {
+  /// The initial state of the `CounterBloc` is 0.
+  CounterBloc() : super(0);
+
+  @override
+  Stream<int> mapEventToState(CounterEvent event) async* {
+    switch (event) {
+
+      /// When a `CounterEvent.increment` event is added,
+      /// the current `state` of the bloc is accessed via the `state` property
+      /// and a new state is emitted via `yield`.
+      case CounterEvent.increment:
+        yield state + 1;
+        break;
+    }
+  }
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final bloc = CounterBloc();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '${bloc.state}',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          bloc.add(CounterEvent.increment);
+          setState(() {});
+        },
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
